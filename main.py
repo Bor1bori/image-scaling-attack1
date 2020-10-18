513f5b421b0c5182c816884fa40f2de56a65ff44
bor1bori <bor1boridv@gmail.com>
2020-10-19 01:33:12 +0900

    Add main.py and write code to start strong_attack

--- main.py
+++ main.py
@@ -3,14 +3,11 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import load_images
+import attack
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    source_img, target_img = load_images.load_color_image_from_disk('./images/cat1.jpg', './images/man1.jpg')
+    attack.strong_attack(lambda x: x, source_img, target_img)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
